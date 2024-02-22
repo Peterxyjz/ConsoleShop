68b8e7ac3cc50bbdb40f2725c563edb30d936b72
phongnqse172516@gmail.com <phongnqse172516@gmail.com>
2024-02-22 13:45:59 +0700

    update ProductFacade - searchByName()

--- consoleShop/src/java/db/ProductFacade.java
+++ consoleShop/src/java/db/ProductFacade.java
@@ -5,10 +5,41 @@
  */
 package db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author ASUS
  */
 public class ProductFacade {
-    
+       public List<Product> searchProductByName(String proName) throws SQLException{
+        Connection con = DBContext.getConnection();
+        PreparedStatement stm = con.prepareStatement("SELECT * FROM Product WHERE proName LIKE ? ");
+        stm.setString(1, proName);
+        
+        //Thực thi lệnh SELECT
+        ResultSet rs = stm.executeQuery();
+        
+           List<Product> proList = new ArrayList<>();
+        
+        while(rs.next()){
+            Product prod = new Product();
+            rs.getString("proID");
+            rs.getString("proName");
+            rs.getFloat("price");
+            rs.getString("categoryID");
+            rs.getString("brandID");
+            rs.getString("description");
+            proList.add(prod);
+            
+        }
+        
+        //trả list Product
+        return proList;
+    }
 }
