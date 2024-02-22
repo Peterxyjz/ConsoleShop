f6e6013feb1d1618a7dc73daff90462de1301936
Peterxyjz <huyq09@gmail.com>
2024-02-22 14:21:43 +0700

    update package

---
+++ consoleShop/src/java/constants/enums.ts
@@ -0,0 +1,28 @@
+export enum UserVerifyStatus {
+  Unverified, // chưa xác thực email, mặc định = 0
+  Verified, // đã xác thực email
+  Banned // bị khóa
+}
+export enum TokenType {
+  AccessToken,
+  RefreshToKen,
+  ForgotPasswordToken,
+  EmailVerificationToken
+}
+
+export enum MediaType {
+  Image,
+  Video
+}
+
+export enum TweetType {
+  Tweet,
+  Retweet,
+  Comment,
+  QuoteTweet
+}
+
+export enum TweetAudience {
+  Everyone,
+  TwitterCircle
+}
