4016def8c4ca382a1329fa7ecc1d397a9b495248
Dematto04 <mlong200004@gmail.com>
2024-03-03 12:13:33 +0700

    adjust product page and footer

--- consoleShop/web/WEB-INF/javascript/product.js
+++ consoleShop/web/WEB-INF/javascript/product.js
@@ -1,5 +1,34 @@
 const moreImgLink = document.getElementById("moreImgLink");
 
 moreImgLink.addEventListener("click", () => {
-  moreImgLink.innerText = "San pham chi co mot hinh"; 
+    moreImgLink.innerText = "San pham chi co mot hinh";
 });
+
+//Change amount
+const plus = document.querySelector(".amount__plus");
+const minus = document.querySelector(".amount__minus");
+const amount = document.getElementById("amount");
+console.log(plus, minus);
+plus.onclick = () => {
+    if (amount.value >= "1") {
+        amount.value = parseInt(amount.value) + 1;
+    } else if (amount.value === "") {
+        amount.value = 1;
+    }
+
+};
+minus.onclick = () => {
+    if (amount.value === "") {
+        amount.value = 1;
+    }
+    if (amount.value >= "2" || amount.value === "") {
+        amount.value = parseInt(amount.value) - 1;
+    }
+
+};
+amount.oninput = ()=>{
+    if(!amount.value.match("^[0-9]+$")){
+        amount.value = "";
+    }
+};
+
