2f75209ccc1d8342f9bef02fdca41096ff1c5719
Dematto04 <mlong200004@gmail.com>
2024-03-06 20:11:30 +0700

    layout update account

---
+++ consoleShop/web/WEB-INF/javascript/accountUpdate.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var btnSubmit = document.querySelector(".personal__submit");
+var phoneInput = document.getElementById("phoneNumber");
+//alert when submit
+btnSubmit.onclick = (event)=>{
+    var answer = window.confirm("Bạn có chắc chắn muốn lưu thông tin đã nhập ?");
+    if(answer){
+
+    }
+    else{
+        event.preventDefault();
+    }
+    
+};
+// phone only accept number
+phoneInput.oninput = (e)=>{
+    const reg = '^[0-9]+$';
+        if(!e.target.value.match(reg)){
+            e.target.value = "";
+        }
+    
+};
+
